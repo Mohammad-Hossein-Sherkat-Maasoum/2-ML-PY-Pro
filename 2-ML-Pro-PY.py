c51342816df479c4e482aba75b304995cd6e6edb
AmirCollider <101126826+Mohammad-Hossein-Sherkat-Maasoum@users.noreply.github.com>
2023-06-12 16:34:15 +0330

    Add files via upload

---
+++ 2-ML-Pro-PY.py
@@ -0,0 +1,16 @@
+#2-ML-Pro-PY
+#میانگین گرفتن یک آرایه یک بعدی
+import numpy as np
+
+a = np.array([1, 2, 3])
+b = np.array([4, 5, 6])
+
+# جمع دو آرایه
+c = a + b
+print(c)
+# ضرب آرایه در عدد
+d = 2 * a
+print(d)
+# محاسبه میانگین آرایه
+mean = np.mean(a)
+print(mean)
